515f91e6c7c5f28ba3d1fa1d8dcde4fd9358f669
MelodyLiss <lisette.jofre.1@gmail.com>
2024-11-09 19:10:36 -0300

    base capa server

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+require('dotenv').config()
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = 3000;
+        this.middlewares();
+        this.routers();
+    }
+
+    middlewares(){
+        this.app.set('view engine','hbs');
+        this.app.use(express.json());
+        hbs.registerPartials(path.join(__dirname,'../views/partials'));
+        this.app.use(express.urlencoded({extended:true}));
+        this.app.use(express.static('public'));
+    }
+
+    routers(){
+
+    }
+
+    listen() {
+        const reset = "\x1b[0m";         
+        const Colorlog = "\x1b[42m"; 
+    
+        this.app.listen(this.port, () => {
+            console.log(`✅${Colorlog} Servidor inicializado en el puerto ${this.port} ${reset}✅ `);
+        });
+    }
+    
+}
+module.exports = Server;
